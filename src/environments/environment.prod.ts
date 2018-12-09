659d3410aa7d0d37f4e470c877759ae3a5529fba
pepe_witch <skpepe134@gmail.com>
2018-12-09 10:52:11 +0700

    add environment setting for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCPwF1MNrFxIJEn0c2elV8uMUDkLbDDVcA',
+    // authDomain: '<your-project-authdomain>',
+    // databaseURL: '<your-database-URL>',
+    projectId: 'myworkspace-4ef9e',
+    // storageBucket: '<your-storage-bucket>',
+    // messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCPwF1MNrFxIJEn0c2elV8uMUDkLbDDVcA',
+    // authDomain: '<your-project-authdomain>',
+    // databaseURL: '<your-database-URL>',
+    projectId: 'myworkspace-4ef9e',
+    // storageBucket: '<your-storage-bucket>',
+    // messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
 
 /*
