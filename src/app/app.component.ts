76e2a0d7768ec26bf24b31fc93edc5f31f061c16
pepe_witch <skpepe134@gmail.com>
2018-12-14 11:02:05 +0700

    config material icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase/app';
-import { OverlayContainer } from '@angular/cdk/overlay';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,27 @@ import { OverlayContainer } from '@angular/cdk/overlay';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  themeClass: string;
-  constructor(private db: AngularFirestore, public afAuth: AngularFireAuth , private overlayContainer: OverlayContainer) {}
-  ngOnInit() {
+  constructor(
+    private http: HttpClient,
+    private db: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer,
+  ) {
+    iconRegistry.addSvgIcon(
+      'menu',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/baseline-menu-24px.svg'),
+    );
   }
+  ngOnInit() {}
   login() {
     const provider = new firebase.auth.GoogleAuthProvider();
     provider.addScope('profile');
     provider.addScope('email');
-    this.afAuth.auth.signInWithPopup(provider).then(e => console.log(e.user)).catch(e => console.log('error' , e));
+    this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(e => console.log(e.user))
+      .catch(e => console.log('error', e));
   }
   lookup() {
     console.log('click!');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,24 @@ import { FirebaseModule } from './modules/firebase.module';
 import { HomeComponent } from './view/home/home.component';
 import { ShowcaseComponent } from './view/showcase/showcase.component';
 import { HeaderComponent } from './component/header/header.component';
+import { NavigationBarComponent } from './component/navigation-bar/navigation-bar.component';
+import { MatIconRegistry } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ShowcaseComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, ShowcaseComponent, HeaderComponent, NavigationBarComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AngularFireModule.initializeApp(environment.firebase),
     FirebaseModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    MatIconRegistry
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
